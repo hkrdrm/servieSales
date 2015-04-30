2b0c0880450aa46c106107616b33db7a4f6ada16
Matt <hkr.drm@gmx.com>
2015-04-29 20:43:52 -0500

    delete bubble on items added

--- public/javascripts/slideToggle.js
+++ public/javascripts/slideToggle.js
@@ -1,12 +1,12 @@
 $(function() {
-    $("td[colspan=4]").find("div[class=hiddenPane]").hide();
+    $("td[colspan=5]").find("div[class=hiddenPane]").hide();
     $("td[class=minus]").click(function(event) {
         event.stopPropagation();
         var $target = $(event.target);
         $target.closest("tr").next().find("div[class=hiddenPane]").slideToggle();
         if ($target.closest("tr").find("span").hasClass("glyphicon-plus-sign"))
-          $target.closest("tr").find("span").removeClass("glyphicon-plus-sign").addClass("glyphicon-minus-sign");
+          $target.closest("tr").find("span.glyphicon").removeClass("glyphicon-plus-sign").addClass("glyphicon-minus-sign");
         else
-          $target.closest("tr").find("span").removeClass("glyphicon-minus-sign").addClass("glyphicon-plus-sign");
+          $target.closest("tr").find("span.glyphicon").removeClass("glyphicon-minus-sign").addClass("glyphicon-plus-sign");
     });
 });
